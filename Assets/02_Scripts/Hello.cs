c3df1c0fda0aa53b1df0c1749eea52a29989109a
Choi Seoyeon <cdawn0808@gmail.com>
2025-12-09 16:52:41 +0900

    First

---
+++ Assets/02_Scripts/Hello.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, world!");
+    }
+
+}
